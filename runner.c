d31a8fef6b97f8d056bfb66191729733baec4b41
BECHERRA Matteo <matteobecherra@gmail.com>
2022-11-04 13:00:38 +0100

    Removed commandLists to prepare uses of readline command

--- runner.c
+++ runner.c
@@ -5,92 +5,6 @@
 #include "command/pwd.c"
 #include "command/exit.c"
 
-//Méthodes de listes de commandes commandList
-
-listNode* buildlistNode(command *c){
-    listNode* ret = malloc(sizeof(listNode));
-    if(ret == NULL){
-        perror("listNode malloc error\n");
-        return NULL;
-    }
-    ret->prev = NULL;
-    ret->next = NULL;
-    ret->cmd = c;
-    return ret;
-}
-
-/**
- * @brief Build a new command List based on the struct \b commandList.
- * Which contains : The List length and the top List node.
- * @return a pointer commandList* to the commandList informations.
- */
-commandList* buildCommandList(){
-    commandList *ret = malloc(sizeof(commandList));
-    if(ret == NULL){
-        perror("commandList malloc error\n");
-        return NULL;
-    }
-    ret->length=0;
-    ret->top = NULL;
-    ret->bottom = NULL;
-    return ret;
-}
-
-/**
- * @brief Pushes a c command on the s commandList .
- * @return 0 if successful, -1 otherwise.
- */
-int commandListPush(commandList* s, command * c){
-    listNode* newTop = buildlistNode(c);
-    if(newTop == NULL) return -1;
-
-    switch (s->length)
-    {
-    case 0 : 
-        s->top = newTop;
-        s->bottom = newTop;
-        s->length++;
-        return 0;
-    
-    default:
-        newTop->prev = s->top;
-        s->top->next = newTop;
-        s->top = newTop;
-        s->length++;
-        return 0;
-    }
-
-    return -1;
-}
-
-/**
- * @brief gets the top command of the command list .
- * @return a command* located at the top of the list or NULL if the list is empty .
- */
-command* commandListPeek(commandList* s){
-    if(s->length == 0){
-        perror("Peek on empty List");
-        return NULL;
-    }
-    return s->top->cmd;
-}
-
-int isEmptyList(commandList* s){
-    return s->length == 0;
-}
-
-void freeList(commandList* s){
-
-    listNode* tmp = s->bottom;
-    while(s->length != 0 && tmp != NULL){
-        free(tmp->cmd);
-        free(tmp->prev);
-        tmp=tmp->next;
-    }
-    free(s->top);
-    free(s->bottom);
-    free(s);
-}
 
 /**
  * @brief Build a new command based on the struct \b command.
@@ -134,18 +48,10 @@ int main(int argc, char *argv[]) {
         perror("problème repertoire courant"); return -1;
     }
 
-    //Création de la pile des commandes
-    commandList* history = buildCommandList();
-    if(history == NULL) {
-        return -1;
-    }
-
     char* tmp[2] = {"pwd","-P"}; 
     command* comm = buildCommand(tmp, 2);
-    //Ajout à l'historique (exemple)
-    commandListPush(history, comm);
+    
     commandProcessHandler(comm, currPath);
 
     free(currPath);
-    freeList(history);
 }
--- runner.h
+++ runner.h
@@ -21,30 +21,4 @@ typedef struct command{
 command* buildCommand(char** command,  int argNumber);
 int commandProcessHandler(command* command, char* currPath);
 
-
-
-
-//Noeud de la pile de commande
-typedef struct listNode listNode;
-typedef struct listNode{
-    command* cmd;
-    listNode* prev;
-    listNode* next;
-} listNode;
-
-//Pile de commande avec le pointeur vers son sommet et sa taille
-typedef struct commandList{
-    int length;
-    listNode* bottom;
-    listNode* top;
-} commandList;
-
-listNode* buildlistNode(command *c);
-commandList* buildCommandList();
-int commandListPush(commandList* s, command * c);
-command* commandListPop(commandList* s);
-command* commandListPeek(commandList* s);
-int isEmptyList(commandList* s);
-void freeList(commandList* s);
-
 #endif
