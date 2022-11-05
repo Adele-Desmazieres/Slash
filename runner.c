333a86d7b098333b4905be579577d36e4bb86f95
Delta-Chat <adele.desmazi@gmail.com>
2022-11-05 16:53:15 +0100

    Main loop et placeholder pour les fonctions de parsing.

--- runner.c
+++ runner.c
@@ -35,25 +35,73 @@ int commandProcessHandler(command* command, char* currPath) {
     return 0;
 }
 
+int countArgs(char* line) {
+    return 2;
+}
+
+char** parseLine(char* line, char** parsedLine) {
+    //char* tmp[2] = {"pwd","-P"};
+    parsedLine[0] = "pwd";
+    parsedLine[1] = "-P";
+    return parsedLine;
+}
+
 int main(int argc, char *argv[]) {
-    char* path = "oulalah/cest/trop/long/osskour/aled/je/meurs";
+    //char* path = "oulalah/cest/trop/long/osskour/aled/je/meurs";
     //char* newPath = reducePathPromptLenght(path);
     //printf("%s", newPath);
 
     //printPrompt(0, path);
     //printf("\n");
+    
+    rl_outstream = stderr; // affichage du prompt sur la sortie erreur, voir sujet
 
-    //A mettre à jour avec chaque appel à cd!
-    char *currPath = malloc(sizeof(char)* MAX_ARGS_STRLEN);
+    int running = 1; 
+
+    char* line = malloc(sizeof(char) * MAX_ARGS_STRLEN);
+    if (line == NULL) { return -1; }
+
+    char* currPath = malloc(sizeof(char) * MAX_ARGS_STRLEN); // a mettre à jour avec chaque appel à cd
     getcwd(currPath, MAX_ARGS_STRLEN);
-    if(currPath == NULL) {
+    if (currPath == NULL) {
         perror("problème repertoire courant"); return -1;
     }
 
+    char** parsedLine = malloc(1 * sizeof(char*)); 
+
+
+    while (running) {
+
+        line = readline("mon test de prompt $ ");
+        add_history(line); 
+        printf("%s", line);
+        printf(" : diplayed\n");
+
+        // parser la ligne (mots et opérateurs séparés par des espaces)
+        int nbrArgs = countArgs(line);
+        printf(" > nbrArgs : %d\n", nbrArgs);
+
+        if (realloc(parsedLine, nbrArgs * sizeof(char *)) == NULL) { printf("nop"); return -1; };
+        //if (parsedLine == NULL) { return -1; }
+        parsedLine = parseLine(line, parsedLine);
+
+        // initier un objet commande
+        command* commande = buildCommand(parsedLine, nbrArgs);
+
+        // interpeter la commande
+        commandProcessHandler(commande, currPath);
+
+        // PLACEHOLDER pour l'arret
+        if (strcmp("exit", line) == 0) { running = 0; }
+    }
+
+    /* OLD TESTS
     char* tmp[2] = {"pwd","-P"}; 
     command* comm = buildCommand(tmp, 2);
     commandProcessHandler(comm, currPath);
     readline( printPrompt(0, path) );
+    */
 
     free(currPath);
+    return 0;
 }
