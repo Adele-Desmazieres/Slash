54b566bb7aea5c53dc0fcf3c76ac7a4deb39b72e
BECHERRA matteo <matteobecherra@gmail.com>
2022-11-06 23:08:27 +0100

    Implemented parsing, changed exit command return values, (PATH_MAX / 4) seems to be max limit for currPath size (core dumped otherwise), fait par Mattéo & Adèle

--- command/exit.c
+++ command/exit.c
@@ -4,13 +4,9 @@
 #define EXIT_C
 
 int exitCommandRunner(command* command) {
-    if (strcmp(command->args[1], "0") != 0 && strcmp(command->args[1], "1")) {
-        command->success = FALSE;
-        printError("Invalid argument for the command exit. Expected argument : 0 or 1.\n");
-        return 0;
-    }
+    if (command->argNumber == 1) return 0;
     int exitCode = atoi(command->args[1]);
-    exit(exitCode);
+    return exitCode;
 }
 
 #endif
--- runner.c
+++ runner.c
@@ -27,23 +27,84 @@ command* buildCommand(char** commandLine, int argNumber) {
     return newCommand;
 }
 
-int commandProcessHandler(command* command, char* currPath) {
-    if ( strcmp(command->name, "exit") == 0 ) return exitCommandRunner(command);
+int* commandProcessHandler(command* command, char* currPath) {
+    int* normalRet = calloc(2 , sizeof(int));
+    if(normalRet == NULL) perror("Erreur calloc");
+    
+    if ( strcmp(command->name, "exit") == 0 ){
+        
+        normalRet[0] = exitCommandRunner(command);
+        
+        normalRet[1] = 0;
+        return normalRet;
+    }
     if ( strcmp(command->name, "cd") == 0 ) cdCommandRunner(command, currPath);
-    if ( strcmp(command->name, "pwd") == 0 ) pwdCommandRunner(command, currPath);
+    if ( strcmp(command->name, "pwd") == 0 ){
+        normalRet[0] = pwdCommandRunner(command, currPath);
+        normalRet[1] = 1;
+        return normalRet;
+    } 
     //TO-DO : cas des commandes externes
-    return 0;
+    
+    normalRet[0] = 0;
+    normalRet[1] = 1;
+    return normalRet;
 }
 
-int countArgs(char* line) {
-    return 2;
+int countArgs(const char* c) {
+    
+    while(!isalnum(*c)){
+        if(*c == '\0') return 0;
+        c++;
+    } 
+    int count = 1;
+    while ( *c != '\0'){
+        if (*c == ' '){
+            count++;
+            while (*c == ' '){
+                if (*c == '\0') return count;
+                c++;
+            } 
+        }
+        c++;
+    }
+    return count;
 }
 
-char** parseLine(char* line, char** parsedLine) {
-    //char* tmp[2] = {"pwd","-P"};
-    parsedLine[0] = "pwd";
-    parsedLine[1] = "-P";
+int sizeOfToken (const char* c){
+    int size = 0;
+    while(*c != '\0' && *c != ' '){
+        size++; c++;
+    }
+    return size;
+}
+
+char** parseLine(const char* line, char** parsedLine) {
+    while(!isalnum(*line)){
+        if (*line == '\0') return parsedLine;
+        line++;
+    }
+    
+    int iterator = 0;
+    while(*line != '\0'){
+        if(*line != ' '){
+            int tokenSize = sizeOfToken(line);
+            parsedLine[iterator] = calloc (sizeof(char) , (1 + tokenSize));
+            strncat(parsedLine[iterator], line, tokenSize);
+            parsedLine[iterator++][tokenSize] = '\0';
+            line += tokenSize;
+            if(*line == '\0') return parsedLine; 
+        }
+        line++;
+    }
+    
     return parsedLine;
+}
+
+void printParsed(char ** parsed, int len){
+    for(int i = 0; i < len; i++){
+        printf(" argument %d : %s \n", i+1, parsed[i]);
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -61,38 +122,54 @@ int main(int argc, char *argv[]) {
     char* line = malloc(sizeof(char) * MAX_ARGS_STRLEN);
     if (line == NULL) { return -1; }
 
-    char* currPath = malloc(sizeof(char) * MAX_ARGS_STRLEN); // a mettre à jour avec chaque appel à cd
-    getcwd(currPath, MAX_ARGS_STRLEN);
+    char* currPath = malloc((PATH_MAX / 4) * sizeof(char)); // a mettre à jour avec chaque appel à cd ; PATH_MAX / 4 car PATH_MAX / 2 trop grand
+    getcwd(currPath, (PATH_MAX / 4));
     if (currPath == NULL) {
         perror("problème repertoire courant"); return -1;
     }
 
-    char** parsedLine = malloc(1 * sizeof(char*)); 
+    char** parsedLine = NULL;
 
+    using_history();
+    int returnValue = 0; 
 
     while (running) {
 
-        line = readline("mon test de prompt $ ");
+        line = readline(printPrompt(returnValue,currPath));
         add_history(line); 
-        printf("%s", line);
-        printf(" : diplayed\n");
+
+        printf("%s  : displayed\n", line);
 
         // parser la ligne (mots et opérateurs séparés par des espaces)
         int nbrArgs = countArgs(line);
         printf(" > nbrArgs : %d\n", nbrArgs);
 
-        if (realloc(parsedLine, nbrArgs * sizeof(char *)) == NULL) { printf("nop"); return -1; };
+        if (parsedLine == NULL) {
+            parsedLine = calloc(nbrArgs , sizeof(char *));
+            if (parsedLine == NULL) perror ("Erreur calloc");
+        } else {
+            if (realloc(parsedLine, nbrArgs * sizeof(char *)) == NULL) { printf("nop"); return -1; };
+        }
+        
         //if (parsedLine == NULL) { return -1; }
+        
         parsedLine = parseLine(line, parsedLine);
-
+        printParsed(parsedLine, nbrArgs);
+        
+        
+        
         // initier un objet commande
         command* commande = buildCommand(parsedLine, nbrArgs);
+        
 
-        // interpeter la commande
-        commandProcessHandler(commande, currPath);
-
-        // PLACEHOLDER pour l'arret
-        if (strcmp("exit", line) == 0) { running = 0; }
+        // interpréter la commande
+        int* stop = commandProcessHandler(commande, currPath) ;
+        
+        if (stop[1] == 0){
+            running = 0;
+        }
+        returnValue = stop[0];
+        
     }
 
     /* OLD TESTS
@@ -101,7 +178,9 @@ int main(int argc, char *argv[]) {
     commandProcessHandler(comm, currPath);
     readline( printPrompt(0, path) );
     */
-
+    
+    free(parsedLine);
+    free(line);
     free(currPath);
-    return 0;
+    return returnValue;
 }
