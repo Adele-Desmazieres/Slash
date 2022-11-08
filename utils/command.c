9f560bcc9b420a0135c7f6f464dd8f761e12bf4b
Akihiro <Jensenmidori@gmail.com>
2022-11-08 01:43:44 +0100

    Adding some docs

--- utils/command.c
+++ utils/command.c
@@ -23,6 +23,18 @@ command* buildCommand(char** commandLine, int argNumber) {
     return newCommand;
 }
 
+/**
+ * @brief Build a new command result based on the struct \b commandResult.
+ * Which contains : 
+ * - \b success       : int that represents the success or not of the command. \newline
+ * - \b fatal         : int that determinate if the execution is fatal for the slash 
+ *                      or not (always FALSE here)
+ * - \b exitCode      : int that will be used to exit the program (unused here.)
+ * - \b resultMessage : string that will be displayed as a result of the command 
+ * @param success the boolean that represents the success or not of the command.
+ * @param resultString The string that will be displayed as a result of the command
+ * @return a pointer commandResult*.
+ */
 commandResult* buildCommandResult(int success, char* resultString) {
     commandResult* newCommandResult = Malloc(sizeof(commandResult), "");
     newCommandResult->exitCode = -1;
@@ -33,6 +45,19 @@ commandResult* buildCommandResult(int success, char* resultString) {
     return newCommandResult;
 }
 
+/**
+ * @brief Build a new command result based on the struct \b commandResult.
+ * Which contains : 
+ * - \b success       : int that represents the success or not of the command. \newline
+ * - \b fatal         : int that determinate if the execution is fatal for the slash 
+ *                      or not (always FALSE here)
+ * - \b exitCode      : int that will be used to exit the program (unused here.)
+ * - \b resultMessage : string that will be displayed as a result of the command 
+ * @param success the boolean that represents the success or not of the command.
+ * @param errorMessage The string that will be displayed as error warning.
+ * @param exitCode the code that will be used to end the program.
+ * @return a pointer commandResult*.
+ */
 commandResult* buildFatalCommandResult(int success, char* errorMessage, int exitCode) {
     commandResult* newCommandResult = Malloc(sizeof(commandResult), "");
     newCommandResult->exitCode = exitCode;
