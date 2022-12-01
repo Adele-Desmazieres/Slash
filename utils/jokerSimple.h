92952d62503384f1af8182044ebda7eb2505cc1f
BECHERRA matteo <matteobecherra@gmail.com>
2022-12-01 13:10:14 +0100

    Genèse du vrai joker *, toujours des erreurs de segmentations

--- utils/jokerSimple.h
+++ utils/jokerSimple.h
@@ -1,17 +1,23 @@
 #ifndef JOKER_SIMPLE_H
 #define JOKER_SIMPLE_H
 
-typedef struct pathArray {
+typedef struct listNode listNode;
+
+typedef struct listNode {
+    char* content;
+    listNode* next;
+} listNode;
+
+typedef struct pathList {
     int len;
-    char** content;
-} pathArray;
+    listNode* first;
+} pathList;
 
 int containsSimpleJoker (const char* path);
-void freePathArray(pathArray* arr);
+void freepathList(pathList* arr);
 int isSuffix(const char* str, const char* suffix);
-char* truncatePath(const char* path);
 char* getSuffix(const char* path);
-pathArray* jokerSimple(const char* str);
+pathList* jokerSimple(char* orPath);
 
 
 #endif
